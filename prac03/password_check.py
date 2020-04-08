177853c1ab1623e0bfbd82b9b3fb4a19784f4157
wintthiri <wintthiriswe@my.jcu.edu.au>
2020-04-08 16:05:09 +0800

    Refactor password check programs to use functions

--- prac03/password_check.py
+++ prac03/password_check.py
@@ -1,6 +1,9 @@
-"""Github link : """
 
-"""Print stars by the number of characters in password"""
+
+"""Print stars by the number of characters in password
+
+Github link: https://github.com/wint-thiri-swe/cp1404practicals/blob/master/prac03/password_check.py
+"""
 
 MIN_LENGTH = 6
 
@@ -15,7 +18,7 @@ def print_asterisk(password):
 
 
 def get_password():
-    password = input(">")
+    password = input("Enter password:")
     while len(password) < MIN_LENGTH:
         print("invalid password, password should be at least 6 character")
         password = input(">")
