e9b0ac9e5ec5752b5ed2ca26f5adda16896a8155
wintthiri <wintthiriswe@my.jcu.edu.au>
2020-04-21 11:15:39 +0800

    Create a program of quick pick lottery generator

---
+++ prac04/quick_picks.py
@@ -0,0 +1,25 @@
+"""
+CP 1404 Practical
+"Quick Pick" Lottery Ticket Generator
+"""
+import random
+
+NUMBER_PER_LINE = 6
+MIN = 1
+MAX = 45
+
+number_of_lines = int(input("How many quick picks? "))
+while number_of_lines < 0:
+    print("Invalid number of quick picks")
+    number_of_lines = int(input("How many quick picks? "))
+
+# for loop to print random
+for i in range(number_of_lines):
+    quick_pick = []
+    for j in range(NUMBER_PER_LINE):
+        number = random.randint(MIN, MAX)
+        while number in quick_pick:
+            number = random.randint(MIN, MAX)
+        quick_pick.append(number)
+    quick_pick.sort()
+    print(" ".join("{:3}".format(number) for number in quick_pick))
