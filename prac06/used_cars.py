385041b1fa85c727e46e7cd0b4c9b1b59a927487
wintthiri <wintthiriswe@my.jcu.edu.au>
2020-05-05 22:41:51 +0800

    Modify main with additional codes

---
+++ prac06/used_cars.py
@@ -0,0 +1,27 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("Ferrari", 180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    # print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car("MINI Cooper", 100)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print("fuel =", limo.fuel)
+    print("odo =", limo.odometer)
+    print(limo)
+
+
+if __name__ == '__main__':
+    main()
